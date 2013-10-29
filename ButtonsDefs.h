c978b1a5972e56fea47624d03c7eb8780d6d6671
Max Johansen <macsj200@problemshnarf.ex.com>
2013-10-29 16:18:10 -0600

    Second initial commit

---
+++ ButtonsDefs.h
@@ -0,0 +1,39 @@
+#pragma systemFile
+//Supress unused warnings
+
+//Definitions for buttons
+const int button_00 = 1;
+const int button_01 = 2;
+const int button_02 = 4;
+const int button_03 = 8;
+const int button_04 = 16;
+const int button_05 = 32;
+const int button_06 = 64;
+const int button_07 = 128;
+const int button_08 = 256;
+const int button_09 = 512;
+const int button_10 = 1024;
+const int button_11 = 2048;
+
+//Definitions for tophat
+const int pov_middle = -1;
+const int pov_north = 0;
+const int pov_south = 4;
+const int pov_west = 6;
+const int pov_east = 2;
+const int pov_northeast = 1;
+const int pov_northwest = 7;
+const int pov_southwest = 5;
+const int pov_southeast = 3;
+
+//Better definitions for buttons
+const int button_x = button_00;
+const int button_y = button_03;
+const int button_a = button_01;
+const int button_b = button_02;
+const int button_left_trigger = button_06;
+const int button_right_trigger = button_07;
+const int button_left_button = button_04;
+const int button_right_button = button_05;
+const int button_back = button_08;
+const int button_start = button_09;
