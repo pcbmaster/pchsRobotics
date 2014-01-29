00c8653a271e590521fdbea04b2916b9b3d94110
Max <macsj200@gmail.com>
2014-01-29 15:39:02 -0700

    works

--- NickMaxTeleOp.c
+++ NickMaxTeleOp.c
@@ -1,21 +1,19 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
+#pragma config(Hubs,  S1, HTMotor,  none,     none,     none)
 #pragma config(Hubs,  S2, HTServo,  none,     none,     none)
 #pragma config(Hubs,  S3, HTMotor,  none,     none,     none)
 #pragma config(Hubs,  S4, HTMotor,  HTMotor,  none,     none)
-#pragma config(Motor,  mtr_S1_C1_1,     fr,            tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     br,            tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,     fl,            tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     bl,            tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     motorH,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_2,     motorI,        tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C4_1,     blockLift,     tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C4_2,     sweeperMotor,  tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S3_C1_1,     motorL,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S3_C1_2,     motorM,        tmotorTetrix, openLoop)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S3,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S4,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C1_1,     fl,            tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     bl,            tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S3_C1_1,     fr,            tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S3_C1_2,     br,            tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_1,     liftA,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_2,     liftB,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_1,     sweeper,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_2,     spinner,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_1,     sweeper,       tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_2,     spinner,       tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S2_C1_1,    cubeLiftServoA,       tServoStandard)
 #pragma config(Servo,  srvo_S2_C1_2,    cubeLiftServoB,       tServoStandard)
 #pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
@@ -83,13 +81,13 @@ task main()
 			motor[br] = -100;
 			motor[bl] = -100;
 		}
-		else if(joystick.joy1_Buttons == button_right_trigger){
+		else if(joystick.joy1_Buttons == button_left_trigger){
 			motor[fr] = 100;
 			motor[fl] = -100;
 			motor[br] = -100;
 			motor[bl] = 100;
 		}
-		else if(joystick.joy1_Buttons == button_left_trigger){
+		else if(joystick.joy1_Buttons == button_right_trigger){
 			motor[fr] = -100;
 			motor[fl] = 100;
 			motor[br] = 100;
